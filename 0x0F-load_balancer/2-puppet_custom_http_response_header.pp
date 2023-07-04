625c3ddd80241d4e27ee6f66de8c8a7d1f38ab8b
Credoecspa <ezejioforchiemerie708@gmail.com>
2023-07-04 13:28:56 +0000

    advance installation

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
-# a Puppet script that it configures a brand new Ubuntu machine to the requirements asked in this task
+# a Puppet script that it configures a brand new Ubuntu machine
 
-exec {'update_package_list':
+exec {'update_package_lists':
   provider => shell,
   command  => 'sudo apt-get -y update',
   before   => Exec['install Nginx'],
@@ -12,14 +12,14 @@ exec {'install Nginx':
   before   => Exec['add_header'],
 }
 
-exec { 'header_display':
+exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
   before      => Exec['restart Nginx'],
 }
 
-exec { 'restart Nginx service':
+exec { 'restart Nginx':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
