a1a9cbc920675b52264d869e0e9e5b828a8ae9f9
Credoecspa <ezejioforchiemerie708@gmail.com>
2023-07-03 17:31:37 -0700

    load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,25 @@
+# a Puppet script that it configures a brand new Ubuntu machine to the requirements asked in this task
+
+exec {'update_package_list':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'header_display':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx service':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
